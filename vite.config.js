83a3616c3cff6c81f32784ca456524cf3fb20059
teatw001 <duydhph26793@fpt.edu.vn>
2022-05-30 04:48:19 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ module.exports = defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         detailElement: resolve(__dirname, 'detail.html'),
-        products: resolve(__dirname, 'product.html')
-        
+        products: resolve(__dirname, 'product.html'),
+        signIns: resolve(__dirname, 'signin.html'),
+        signups: resolve(__dirname, 'signup.html'),
       },
     },
   },
