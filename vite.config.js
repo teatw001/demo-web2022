829c40169e133348d49ebdf54be384eb5ab769a1
teatw001 <duydhph26793@fpt.edu.vn>
2022-05-30 04:45:16 +0700

    update news

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         detailElement: resolve(__dirname, 'detail.html'),
-        products: resolve(__dirname, 'products.html')
+        products: resolve(__dirname, 'product.html')
         
       },
     },
