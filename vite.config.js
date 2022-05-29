637601ac1a7cda17719a71ca18b115ba078208ad
teatw001 <duydhph26793@fpt.edu.vn>
2022-05-30 03:39:19 +0700

    update

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        detailElement: resolve(__dirname, 'detail.html'),
+        products: resolve(__dirname, 'products.html')
+        
+      },
+    },
+  },
+});
